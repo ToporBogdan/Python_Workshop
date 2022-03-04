7b96259be6372a29c78166d2b45bfe7cd41914e9
ToporBogdan <bogdantopor98@gmail.com>
2022-03-04 23:03:59 +0200

    Added ceva nush ce sa zic

---
+++ ceva.py
@@ -0,0 +1,34 @@
+# print("Hi mom")
+# print("Bye mom")
+# for x in range(0, 10):
+#     print(x)
+    # if x > 100:
+    #     print(x)
+    # else:
+    #     if x < 0:
+    #             print(0)
+    # if x < 3:
+    #     print("e bun")
+    # elif x > 3 and x < 6:
+    #     print ("asa si asa")
+    # else:
+    #     print("nu-i bun")
+# # x = ['Hatz', 'Mama', 'Tata']
+# # print(x)
+
+# y = [1, 2, 1, 4]
+# print(set(y))
+
+def isOdd(x):
+    return x % 2
+
+for y in range(10):
+    print(isOdd(y))
+
+
+
+
+
+
+
+
