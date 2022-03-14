d2199daed18e403d0144349e416dc42efc455108
ToporBogdan <bogdantopor98@gmail.com>
2022-03-14 21:46:47 +0200

    Added ex8

---
+++ ex8.py
@@ -0,0 +1,27 @@
+'''
+Să se afișeze toate permutările circulare a cifrelor unui număr.
+
+Exemplu:
+Pentru numărul 12345 se va afișa:
+51234
+45123
+34512
+23451
+12345
+
+Hint: Gândește-te cum rezolvi problema pentru un string, apoi extinzi la cifre ale unui număr.
+'''
+
+numar = 123456789
+
+def Permutari(numar):
+    numar = str(numar)
+    CarSeparat = list(numar.strip())
+    for i in range(len(numar)):
+        aux1 = CarSeparat.pop()
+        CarSeparat.insert(0, aux1)
+        numar = CarSeparat
+        print(int("".join(numar)))
+
+print("Să se afișeze toate permutările circulare a cifrelor numărului:" + str(numar))
+Permutari(numar)
