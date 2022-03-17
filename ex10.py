7c1e047109db36631faf6a307b4e540d173e3304
ToporBogdan <bogdantopor98@gmail.com>
2022-03-17 12:52:38 +0200

    Added ex10 where we tested my skills

---
+++ ex10.py
@@ -0,0 +1,25 @@
+
+from pprint import pprint
+
+x = [1, 3, 5, 1, 34]
+y = (2.3, 5.0)
+z = ["baba", 32, 4j, True, ]
+a = [("mama", "tata"), (23, 42)]
+b = [("Rata", 21, 4j), ("Stb", 3.0)]
+# print(a[1])
+# c = b[0]
+# print(c[2])
+# print(b[0][2])
+# print((2 + 3) * 10)
+l = [(1, 5, 1), (6, 6, 6), (1, 5, 1), ("Mama", 1)]
+# print(set(l))
+matrice = [[1, 3, 5, 1, 1, 4, 6],
+           [1, 1, 1, 123, 523, 1],
+           [1, 6, 2, 5453, 121, 56, 62, 1]]
+# pprint(matrice)
+dick = {
+    "Ford": [("Motor", 1.6), ("Culoare", "Rosu"), ("Clima", True)],
+    "Peugeot": [("Motor", 1.2), ("Culoare", "Cacaniu"), ("Clima", False)],
+    "Mertz": [("Motor", 1.4), ("Culoare", "Galben"), ("Clima", True)]
+}
+pprint(dick)
